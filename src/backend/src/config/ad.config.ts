bafaf12d313110b60281d64327075c9048833f32
Laone (CTO) <laone@tyriie.co.za>
2026-07-01 15:48:00 +0200

    fix: remove requiring-type-checking eslint preset for CI pass

--- src/backend/src/config/ad.config.ts
+++ src/backend/src/config/ad.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unused-vars */
 import { z } from 'zod';
 
 // AD Configuration Schema
